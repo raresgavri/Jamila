38a61cf84eff33aa0b209985dd599e6657e2e0e5
Rares Gavriluta <79149944+raresgavri@users.noreply.github.com>
2022-12-02 02:27:24 +0200

    beautify

--- general.py
+++ general.py
@@ -4,9 +4,6 @@ from urllib.request import urlopen
 from ingredients_finder import IngredientFinder
 
 
-
-
-
 def gather_ingredients(page_url):
     html_string = ''
     try:
@@ -21,3 +18,24 @@ def gather_ingredients(page_url):
         print("Can't crawl page")
         return dict()
     return finder.recipe_ingredients()
+
+
+def load_previous_cart():
+    basket = dict()
+    if os.path.exists('shopping_list.json'):
+        with open("shopping_list.json", "r") as f:
+            data = f.read()
+
+        # decoding the JSON to dictionay
+        basket = json.loads(data)
+        print('Previous cart loaded')
+    return basket
+
+
+def write_to_json(dictionary):
+    # encoding to JSON
+    data = json.dumps(dictionary)
+    print(data)
+    # write to a file
+    with open("shopping_list.json", "w") as f:
+        f.write(data)
--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 from general import *
 import re
-import json
 from fractions import Fraction
 
-basket = dict()
 
-
-if os.path.exists('shopping_list.json'):
-    with open("shopping_list.json", "r") as f:
-        data = f.read()
-
-    # decoding the JSON to dictionay
-    basket = json.loads(data)
-    print('Previous cart loaded')
-
+basket = load_previous_cart()
 
 with open('recipes.txt', 'r') as input_file:
     lines = input_file.read().splitlines()
@@ -25,7 +15,8 @@ for i in range(0, len(lines)):
         if i not in basket:
             basket[i] = current_cart[i]
         else:
-            if basket[i]['unit'] is None:  basket[i]['amount'] = basket[i]['amount'] + ' + ' + (current_cart[i]['amount'])
+            if basket[i]['unit'] is None:
+                basket[i]['amount'] = basket[i]['amount'] + ' + ' + (current_cart[i]['amount'])
             if basket[i]['unit'] == current_cart[i]['unit'] and basket[i]['unit']:
                 if basket[i]['amount'] is not int:
                     basket[i]['amount'] = float(Fraction(basket[i]['amount'])) + float(
@@ -36,9 +27,4 @@ for i in range(0, len(lines)):
                 pass
 
 
-# encoding to JSON
-data = json.dumps(basket)
-print(data)
-# write to a file
-with open("shopping_list.json", "w") as f:
-    f.write(data)
+write_to_json(basket)
